33eae98d507f8e7ff579fabec8ebaa762be7d410
Blacdimir Rosario <blacdimirr@gmail.com>
2025-12-15 11:41:22 -0400

    Mis cambios en DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,16 +11,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        if (\Request::route()->getName() != 'LaravelUpdater::database') {
-            $this->call(UsersTableSeeder::class);
-            $this->call(NotificationSeeder::class);
-            $this->call(AiTemplateSeeder::class);
-            Artisan::call('module:migrate LandingPage');
-            Artisan::call('module:seed LandingPage');
-        } else {
-            Utility::languagecreate();
-        }
-    }
+public function run(): void
+{
+    $this->call(UsersTableSeeder::class);
+    $this->call(NotificationSeeder::class);
+    $this->call(AiTemplateSeeder::class);
+    Artisan::call('module:migrate LandingPage');
+    Artisan::call('module:seed LandingPage');
+}
+
 }
